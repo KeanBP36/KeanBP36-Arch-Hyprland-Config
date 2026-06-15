6af573d753b4c0b82cd5cf2e6cab969b75897013
KeanBP36 <keanpieterse36@gmail.com>
2026-06-15 20:08:27 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-# 1. Update system and install required apps
+set -e
+
+DOTFILES_REPO="https://github.com/youruser/dotfiles.git"  # <-- set this
+
+echo "Updating system and installing packages..."
 sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm firefox kitty rofi-wayland waybar fastfetch hyprlock zsh awww
+sudo pacman -S --noconfirm git firefox kitty rofi-wayland waybar fastfetch hyprlock swww zsh
 
-# 2. Make sure .config exists
-mkdir -p ~/.config
+if [ ! -d "$HOME/dotfiles" ]; then
+    echo "Dotfiles not found. Cloning..."
+    git clone "$DOTFILES_REPO" "$HOME/dotfiles"
+fi
 
-# 3. Create symlinks (Force overwrite)
-ln -sf ~/dotfiles/hypr ~/.config/hypr
-ln -sf ~/dotfiles/waybar ~/.config/waybar
-ln -sf ~/dotfiles/rofi ~/.config/rofi
-ln -sf ~/dotfiles/fastfetch ~/.config/fastfetch
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+mkdir -p "$HOME/.config"
+
+echo "Creating symlinks..."
+ln -sf "$HOME/dotfiles/hypr"       "$HOME/.config/hypr"
+ln -sf "$HOME/dotfiles/waybar"     "$HOME/.config/waybar"
+ln -sf "$HOME/dotfiles/rofi"       "$HOME/.config/rofi"
+ln -sf "$HOME/dotfiles/fastfetch"  "$HOME/.config/fastfetch"
+ln -sf "$HOME/dotfiles/.zshrc"     "$HOME/.zshrc"
+ln -sf "$HOME/dotfiles/.bashrc"    "$HOME/.bashrc"
 
 echo "Install complete! Please restart your terminal or log out/in."
