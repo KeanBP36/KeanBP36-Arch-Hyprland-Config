3a8d1611b7bc238e03a5c04023ca4c6e266aac90
KeanBP36 <keanpieterse36@gmail.com>
2026-06-15 22:13:56 +0200

    Refactor install.sh to use DEST_DIR variable
    Updated the installation script to use a destination directory variable for cloning and symlink creation.

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@
 set -e
 
 # --- Configuration ---
-# Update this with your actual repo
-DOTFILES_REPO="https://github.com/KeanBP36/KeanBP36-Arch-Hyprland-Config.git"
+# Ensure this matches the folder name created by git clone
+# If it clones as 'KeanBP36-Arch-Hyprland-Config', change this:
+DEST_DIR="$HOME/dotfiles"
 
-echo "Cleaning up old configurations to force overwrite..."
-# Force remove existing configs to prevent conflicts
+echo "Cleaning up old configurations..."
+# Remove only if they are not already the correct symlinks
 rm -rf "$HOME/.config/hypr"
 rm -rf "$HOME/.config/waybar"
 rm -rf "$HOME/.config/rofi"
 rm -rf "$HOME/.config/fastfetch"
-rm -rf "$HOME/.config/hyprland" # Removing this to fix your specific error
 rm -f "$HOME/.zshrc"
 rm -f "$HOME/.bashrc"
 
@@ -19,22 +19,23 @@ echo "Updating system and installing packages..."
 sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm git firefox kitty rofi-wayland waybar fastfetch hyprlock swww zsh
 
-if [ ! -d "$HOME/dotfiles" ]; then
+if [ ! -d "$DEST_DIR" ]; then
     echo "Cloning dotfiles..."
-    git clone "$DOTFILES_REPO" "$HOME/dotfiles"
+    git clone "https://github.com/KeanBP36/KeanBP36-Arch-Hyprland-Config.git" "$DEST_DIR"
 else
     echo "Dotfiles already exist. Pulling latest changes..."
-    cd "$HOME/dotfiles" && git pull
+    cd "$DEST_DIR" && git pull
 fi
 
 mkdir -p "$HOME/.config"
 
 echo "Creating symlinks..."
-ln -sf "$HOME/dotfiles/hypr"       "$HOME/.config/hypr"
-ln -sf "$HOME/dotfiles/waybar"      "$HOME/.config/waybar"
-ln -sf "$HOME/dotfiles/rofi"        "$HOME/.config/rofi"
-ln -sf "$HOME/dotfiles/fastfetch"   "$HOME/.config/fastfetch"
-ln -sf "$HOME/dotfiles/.zshrc"      "$HOME/.zshrc"
-ln -sf "$HOME/dotfiles/.bashrc"     "$HOME/.bashrc"
+# -sfn ensures we link correctly and don't create links-inside-links
+ln -sfn "$DEST_DIR/hypr"      "$HOME/.config/hypr"
+ln -sfn "$DEST_DIR/waybar"    "$HOME/.config/waybar"
+ln -sfn "$DEST_DIR/rofi"      "$HOME/.config/rofi"
+ln -sfn "$DEST_DIR/fastfetch" "$HOME/.config/fastfetch"
+ln -sf  "$DEST_DIR/.zshrc"    "$HOME/.zshrc"
+ln -sf  "$DEST_DIR/.bashrc"   "$HOME/.bashrc"
 
 echo "Install complete! Please restart your terminal or log out/in."
